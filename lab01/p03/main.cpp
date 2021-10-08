a9214fc581a02f439de7f2cebfac416c7991922d
Aikokul Tashpulatova <tashpulatova_a@auca.kg>
2021-10-08 06:35:49 +0200

    lab01 p03 completed

--- lab01/p03/main.cpp
+++ lab01/p03/main.cpp
@@ -1,3 +1,4 @@
+#include <iomanip> //Sets the decimal precision to used to format floating-point values (header for setprecision())
 #include <iostream>
 #include <string>
 
@@ -5,11 +6,27 @@ using namespace std;
 
 int main()
 {
-    string name;
-    int age;
+    const int kNameWidth = 30;
+    const int kAvgWidht = 40;
 
-    while (cin >> name >> age)
+    cout << setfill('.') << fixed;
+    for (string name; cin >> name;)
     {
-        cout << name << " " << age << "\n";
+        double sum = 0;
+        int nGrades = 0;
+        for (int grade; cin >> grade;)
+        {
+            sum += grade;
+            ++nGrades;
+        }
+        if (nGrades != 0)
+        {
+            cout << left << setw(kNameWidth) << name << "|" << right << setw(kAvgWidht) << setprecision(2) << sum / nGrades << "\n";
+        }
+        else
+        {
+            cout << left << setw(kNameWidth) << name << "|" << right << setw(kAvgWidht + 1) << "no data\n";
+        }
+        cin.clear();
     }
 }
