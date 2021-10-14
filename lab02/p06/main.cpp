98b493c756733a27af782a24dcc1d020756f4f2d
Aikokul Tashpulatova <tashpulatova_a@auca.kg>
2021-10-14 09:19:47 +0200

    add p06

--- lab02/p06/main.cpp
+++ lab02/p06/main.cpp
@@ -9,11 +9,25 @@ int main()
 {
     iostream::sync_with_stdio(false);
 
-    int a;
-    cin >> a;
+    int testCases;
+    cin >> testCases;
 
-    int b;
-    cin >> b;
+    for (int test = 0; test < testCases; test++)
+    {
+        int salaries[3];
+        cin >> salaries[0] >> salaries[1] >> salaries[2];
 
-    cout << a + b << "\n";
+        int m = max(max(salaries[0], salaries[1]), salaries[2]);
+        int n = min(min(salaries[0], salaries[1]), salaries[2]);
+
+        int r;
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (salaries[i] > n && salaries[i] < m)
+                r = salaries[i];
+        }
+
+        cout << "Case " << test + 1 << ": " << r << "\n";
+    }
 }
