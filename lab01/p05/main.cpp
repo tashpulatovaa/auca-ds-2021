119b338433bbbdc9c81bfed804e97b8206c4a657
Aikokul Tashpulatova <tashpulatova_a@auca.kg>
2021-10-17 09:08:24 +0200

    lab01 p05 completed

--- lab01/p05/main.cpp
+++ lab01/p05/main.cpp
@@ -2,6 +2,35 @@
 #include <iostream>
 using namespace std;
 
+void badSwap(int x, int y)
+{
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+void goodSwap01(int *p, int *q)
+{
+    int temp = *p;
+    *p = *q;
+    *q = temp;
+}
+
+void goodSwap02(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+void auSwap(T &x, T &y)
+{
+    T t = x;
+    x = y;
+    y = t;
+}
+
 int main()
 {
     cout << "1st int: ";
@@ -12,6 +41,7 @@ int main()
     int y;
     cin >> y;
 
+    cout << "std::swap:\n";
     cout << "Before swap "
          << "a = " << x << ", "
          << "b = " << y << endl;
@@ -21,4 +51,46 @@ int main()
     cout << "After swap "
          << "a = " << x << ", "
          << "b = " << y << endl;
+
+    cout << "badSwap\n";
+    cout << "Before swap "
+         << "a = " << x << ", "
+         << "b = " << y << endl;
+
+    badSwap(x, y);
+    cout << "After swap\n "
+         << "a = " << x << ", "
+         << "b = " << y << endl;
+
+    cout << "goodSwap01\n";
+    cout << "Before swap "
+         << "a = " << x << ", "
+         << "b = " << y << endl;
+
+    goodSwap01(&x, &y);
+    cout << "After swap "
+         << "a = " << x << ", "
+         << "b = " << y << endl;
+
+    cout << "goodSwap02\n";
+    cout << "Before swap "
+         << "a = " << x << ", "
+         << "b = " << y << endl;
+
+    goodSwap02(x, y);
+    cout << "After swap "
+         << "a = " << x << ", "
+         << "b = " << y << endl;
+
+    cout << "general swap for doubles:\n";
+    double d1 = 2.32;
+    double d2 = 1.45;
+    cout << "Before swap "
+         << "d1 = " << d2 << ", "
+         << "d2 = " << d2 << endl;
+
+    auSwap(d1, d2);
+    cout << "After swap "
+         << "d1 = " << d1 << ", "
+         << "d2 = " << d2 << endl;
 }
