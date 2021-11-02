001a319671fd1d9faef9d7d7321c74439de3a034
Aikokul Tashpulatova <tashpulatova_a@auca.kg>
2021-11-02 12:47:25 +0100

    lab02 p21 try

--- lab02/p21/main.cpp
+++ lab02/p21/main.cpp
@@ -4,44 +4,39 @@ template <typename C>
 int sz(const C &c) { return static_cast<int>(c.size()); }
 
 using namespace std;
-
+const int START = 1;
 int main()
 {
     iostream::sync_with_stdio(false);
 
-    vector<vector<int>> triples;
-    vector<int> line;
-    int x;
-    int c = 0;
-    while (cin >> x)
+    int start, height, end;
+    vector<int> heights(10000);
+
+    int largeEnd = 0;
+
+    while (cin >> start >> height >> end)
     {
-        c++;
-        for (int i = 0; i < 3; i++)
+        if (largeEnd < end)
         {
-            cin >> triples[c][i];
+            largeEnd = end;
+        }
+
+        for (int i = start; i < end; i++)
+        {
+            heights[i] = max(heights[i], height);
         }
     }
 
-    for (int i = 0; i < sz(triples); i++)
-    {
-        if (i != 0 && line.back() > triples[i][0])
-        {
-            continue;
-        };
-        line.push_back(triples[i][0]);
-        line.push_back(triples[i][1]);
-        int highest = triples[i][1];
-        int highestIndex = i;
-        int l = 0;
+    int best = heights[1];
+    cout << "1 " << best;
 
-        while (triples[i][2] > triples[i + l][0])
+    for (int j = 2; j < largeEnd; ++j)
+    {
+        if (best != heights[j])
         {
-            if (highest < triples[i + l][1])
-            {
-                highestIndex = i + l;
-            }
+            cout << ' ' << ' ' << heights[j];
+            best = heights[j];
         }
-        line.push_back(triples[highest][0]);
-        line.push_back(triples[highest][1]);
     }
+    cout << ' ' << largeEnd << " 0\n";
 }
