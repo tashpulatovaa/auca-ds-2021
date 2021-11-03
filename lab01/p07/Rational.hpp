72e92abb4fb38b55bfb94de81b04b2b0323d3737
Aikokul Tashpulatova <tashpulatova_a@auca.kg>
2021-11-03 10:47:55 +0100

    add lab01 p07

--- lab01/p07/Rational.hpp
+++ lab01/p07/Rational.hpp
@@ -14,6 +14,33 @@ public:
         : mNum(0), mDen(1)
     {
     }
+    Rational(const T &num, const T &den)
+        : mNum(num), mDen(den)
+    {
+        if (mDen == 0)
+        {
+            throw std::runtime_error("Ratinal: denominator is equal to zero");
+        }
+
+        //euclid's algorithm
+        T a = mNum < 0 ? -mNum : mNum;
+        T b = mDen < 0 ? -mDen : mDen;
+        while (b != 0)
+        {
+            T r = a % b;
+            a = b;
+            b = r;
+        }
+
+        mNum /= a;
+        mDen /= a;
+
+        if (mDen < 0)
+        {
+            mNum = -mNum;
+            mDen = -mDen;
+        }
+    }
 
     const T &num() const
     {
@@ -24,6 +51,79 @@ public:
         return mDen;
     }
 };
+
+template <typename T>
+Rational<T> operator+(const Rational<T> &a, const Rational<T> &b)
+{
+    T num = a.num() * b.den() + b.num() * a.den();
+    T den = a.den() * b.den();
+
+    return Rational<T>(num, den);
+}
+
+template <typename T>
+Rational<T> operator-(const Rational<T> &a, const Rational<T> &b)
+{
+    T num = a.num() * b.den() - b.num() * a.den();
+    T den = a.den() * b.den();
+
+    return Rational<T>(num, den);
+}
+
+template <typename T>
+Rational<T> operator*(const Rational<T> &a, const Rational<T> &b)
+{
+    T num = a.num() * b.num();
+    T den = a.den() * b.den();
+
+    return Rational<T>(num, den);
+}
+
+template <typename T>
+Rational<T> operator/(const Rational<T> &a, const Rational<T> &b)
+{
+    T num = a.num() * b.den();
+    T den = a.den() * b.num();
+
+    return Rational<T>(num, den);
+}
+template <typename T>
+bool operator<(const Rational<T> &a, const Rational<T> &b)
+{
+    return a.num() * b.den() < b.den() * a.den();
+}
+
+//a < b <=> b > a
+template <typename T>
+bool operator>(const Rational<T> &a, const Rational<T> &b)
+{
+    return b < a;
+}
+
+template <typename T>
+bool operator==(const Rational<T> &a, const Rational<T> &b)
+{
+    return a.num() * b.den() == b.den() * a.den();
+}
+
+template <typename T>
+bool operator!=(const Rational<T> &a, const Rational<T> &b)
+{
+    return !(a == b);
+}
+
+//a <= b <=> !(a > b)
+template <typename T>
+bool operator<=(const Rational<T> &a, const Rational<T> &b)
+{
+    return !(a > b);
+}
+
+template <typename T>
+bool operator>=(const Rational<T> &a, const Rational<T> &b)
+{
+    return !(a < b);
+}
 
 template <typename T>
 std::ostream &operator<<(std::ostream &out, const Rational<T> &r)
--- lab01/p07/main.cpp
+++ lab01/p07/main.cpp
@@ -10,14 +10,134 @@ using namespace std;
 
 TEST_CASE("Rational: default constructor")
 {
-    int x;
-
     Rational<int> r;
 
     ostringstream sout;
     sout << r;
 
     REQUIRE(sout.str() == "0/1");
+}
+
+TEST_CASE("Rational: constructor with numerator and denominator")
+{
+    ostringstream sout;
+
+    SUBCASE("Test with 1/2")
+    {
+        Rational<int> r(1, 2);
+        sout << r;
+        REQUIRE(sout.str() == "1/2");
+    }
+
+    SUBCASE("Test with 3/2")
+    {
+        Rational<int> r(3, 2);
+        sout << r;
+        REQUIRE(sout.str() == "3/2");
+    }
+
+    SUBCASE("Test with 30/25")
+    {
+        Rational<int> r(30, 25);
+        sout << r;
+        REQUIRE(sout.str() == "6/5");
+    }
+
+    SUBCASE("Test with 17/31")
+    {
+        Rational<int> r(17, 31);
+        sout << r;
+        REQUIRE(sout.str() == "17/31");
+    }
+    SUBCASE("Test with -30/-20")
+    {
+        Rational<int> r(-30, -20);
+        sout << r;
+        REQUIRE(sout.str() == "3/2");
+    }
+    SUBCASE("Test with 30/-20")
+    {
+        Rational<int> r(30, -20);
+        sout << r;
+        REQUIRE(sout.str() == "-3/2");
+    }
+
+    SUBCASE("Test with 3/0")
+    {
+        REQUIRE_THROWS_AS(Rational<int> r(3, 0), runtime_error);
+    }
+}
+
+TEST_CASE("Rational: addition")
+{
+    ostringstream sout;
+    SUBCASE("1/2 + 1/3")
+    {
+        Rational<int> r1(1, 2);
+        Rational<int> r2(1, 3);
+
+        sout << r1 + r2;
+
+        REQUIRE(sout.str() == "5/6");
+    }
+}
+
+TEST_CASE("Rational: subtraction")
+{
+    ostringstream sout;
+    SUBCASE("1/2 - 1/3")
+    {
+        Rational<int> r1(1, 2);
+        Rational<int> r2(1, 3);
+
+        sout << r1 - r2;
+
+        REQUIRE(sout.str() == "1/6");
+    }
+}
+TEST_CASE("Rational: multiplication")
+{
+    ostringstream sout;
+    SUBCASE("1/2 * 1/3")
+    {
+        Rational<int> r1(1, 2);
+        Rational<int> r2(1, 3);
+
+        sout << r1 * r2;
+
+        REQUIRE(sout.str() == "1/6");
+    }
+    // SUBCASE("1/2 * 0/2")
+    // {
+    //     Rational<int> r1(1, 2);
+    //     Rational<int> r2(0, 2);
+
+    //     sout << r1 * r2;
+
+    //     REQUIRE(sout.str() == "0/4");
+    // }
+}
+TEST_CASE("Rational: divition")
+{
+    ostringstream sout;
+    SUBCASE("1/2 / 1/3")
+    {
+        Rational<int> r1(1, 2);
+        Rational<int> r2(1, 3);
+
+        sout << r1 / r2;
+
+        REQUIRE(sout.str() == "3/2");
+    }
+    SUBCASE("1/2 / 0/3")
+    {
+        Rational<int> r1(1, 2);
+        Rational<int> r2(0, 3);
+
+        //sout << r1 / r2;
+
+        REQUIRE_THROWS_AS(r1 / r2, runtime_error);
+    }
 }
 
 TEST_CASE("Rational: getters")
