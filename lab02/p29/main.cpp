989976cc7393f2da16540d580fcff42485dc653a
Aikokul Tashpulatova <tashpulatova_a@auca.kg>
2021-11-07 13:59:53 +0100

    lab02 p29 wrong answer

---
+++ lab02/p29/main.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+int main()
+{
+    iostream::sync_with_stdio(false);
+
+    vector<string> block;
+    for (string line; getline(cin, line) && line != "#";)
+    {
+        if (line.substr(0, 1) == "e")
+        {
+            int maxMatchLine = 0;
+            int maxMatch = 0;
+            for (int i = 0; i < sz(block); i++)
+            {
+                int matchSum = 0;
+                for (int j = 0; j < sz(block); j++)
+                {
+                    if (j == i)
+                        continue;
+                    for (int k = 0; k < 5; k++)
+                    {
+                        if (block[i].substr(k * 4, 3) == block[j].substr(k * 4, 3))
+                        {
+                            matchSum++;
+                        }
+                    }
+                }
+                if (matchSum > maxMatch)
+                {
+                    maxMatch = matchSum;
+                    maxMatchLine = i;
+                }
+            }
+            cout << maxMatchLine + 1 << endl;
+            block.clear();
+            continue;
+        }
+        else
+        {
+            block.push_back(line);
+        }
+    }
+}
