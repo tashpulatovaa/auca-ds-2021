650393df2b1ad49afd19b3a856dcfff9ca46e18a
Aikokul Tashpulatova <tashpulatova_a@auca.kg>
2021-12-09 14:31:27 +0100

    project02 p02 completed

--- project02/p02/main.cpp
+++ project02/p02/main.cpp
@@ -5,7 +5,54 @@ int sz(const C &c) { return static_cast<int>(c.size()); }
 
 using namespace std;
 
+struct Skill
+{
+    string mSkill;
+    int mSalary;
+
+    Skill(string skill, int salary)
+        : mSkill(skill), mSalary(salary)
+    {
+    }
+};
+
 int main()
 {
     iostream::sync_with_stdio(false);
+
+    int nSkills, nTexts;
+    cin >> nSkills >> nTexts;
+    vector<Skill> skills;
+
+    for (int i = 0; i < nSkills; i++)
+    {
+        string skill;
+        int salary;
+
+        cin >> skill >> salary;
+
+        skills.push_back(Skill(skill, salary));
+    }
+
+    sort(begin(skills), end(skills), [](Skill skill1, Skill skill2)
+         { return skill1.mSkill < skill2.mSkill; });
+
+    int result = 0;
+    string skill;
+
+    while (cin >> skill)
+    {
+        if (skill == ".")
+        {
+            cout << result << endl;
+            result = 0;
+        }
+
+        auto it = lower_bound(begin(skills), end(skills), skill, [](Skill a, string b)
+                              { return a.mSkill < b; });
+        if (it != skills.end() && (it->mSkill == skill))
+        {
+            result += (it->mSalary);
+        }
+    }
 }
